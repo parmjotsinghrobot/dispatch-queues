c1f85082dfe3dc6abe79c3baa21555029092ac8b
Parmjot Singh <parmjotsinghrobot@gmail.com>
2026-05-12 08:27:41 +1200

    chore: add comment describing num_cores

--- num_cores.c
+++ num_cores.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
- 
+
+// counts the number of logical cores on the machine and prints it out
 int main() {
     int cores = sysconf(_SC_NPROCESSORS_ONLN);
     printf("This machine has %d cores.\n", cores);
